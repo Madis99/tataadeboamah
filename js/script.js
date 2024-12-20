ef9b5bf0029e502697f5dac8729d2f44e729208f
Madis99 <126186020+Madis99@users.noreply.github.com>
2024-12-20 06:13:27 -0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(function(){
+
+    $(".notification-bar").delay(1000).slideDown().delay(5000).fadeOut();
+    
+
+
+
+    $("#birthdate").datepicker({
+        changeMonth: true,
+        changeYear: true,
+});
+
+//window.alert("Please wait...");
